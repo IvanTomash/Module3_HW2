df4c65123fff648d2288dfc5729fde59c1dceecd
IvanTomash <tomashuckvanya@gmail.com>
2023-08-27 21:01:14 +0300

    First commit HW_3_2

---
+++ Homework_3_2/Homework_3_2/Contact.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Homework_3_2
+{
+    /// <summary>
+    /// Represents phone contacts.
+    /// </summary>
+    internal sealed class Contact : IComparable<Contact>
+    {
+        private string name;
+
+        private string number;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="number">Accepted phone number.</param>
+        /// <param name="name">Accepted name.</param>
+        public Contact(string number, string name = "")
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        /// <summary>
+        /// Gets a name.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            string res = $"{this.name} {this.number}";
+            return res;
+        }
+
+        /// <inheritdoc/>
+        public int CompareTo(Contact other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Homework_3_2/Homework_3_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Homework_3_2
+{
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    internal sealed class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">Args.</param>
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ContactCollection contacts = new ContactCollection();
+            contacts.AddContact(new Contact("0983156712", "Іван"), new CultureInfo("en-US"));
+            contacts.AddContact(new Contact("0949049876", "Ivan"));
+            contacts.AddContact(new Contact("0689873428", "Nick"), new CultureInfo("uk-UA"));
+            contacts.AddContact(new Contact("0689873428", "Ann"), new CultureInfo("uk-UA"));
+            contacts.AddContact(new Contact("0983156712", "Andrew"), new CultureInfo("en-US"));
+            contacts.AddContact(new Contact("0949049876", "029Kiril"));
+            contacts.AddContact(new Contact("0689873428", "Олексій"), new CultureInfo("uk-UA"));
+            contacts.AddContact(new Contact("0689873428", "Вікторія"), new CultureInfo("uk-UA"));
+
+            foreach (var contact in contacts.SortedContacts)
+            {
+                Console.WriteLine(contact.Key);
+                foreach (var c in contact.Value)
+                {
+                    Console.WriteLine($"{c}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
